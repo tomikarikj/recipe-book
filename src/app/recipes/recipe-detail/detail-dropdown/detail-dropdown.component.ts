ea4239b266a06c850f359ccccb7adbc8af92d6a7
Tomi Karikj <karikjtomi@gmail.com>
2019-02-04 21:04:04 +0100

    Created separate components for the dropdown buttons

---
+++ src/app/recipes/recipe-detail/detail-dropdown/detail-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-dropdown',
+  templateUrl: './detail-dropdown.component.html',
+  styleUrls: ['./detail-dropdown.component.css']
+})
+export class DetailDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
