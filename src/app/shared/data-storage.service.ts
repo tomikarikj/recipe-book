586cd5108699456e0cb496cb32311c634a202e3b
Tomi Karikj <karikjtomi@gmail.com>
2019-02-10 20:49:56 +0100

    Implemented the auth service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,15 +3,21 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
+    const token = this.authService.getToken();
     const req = new HttpRequest(
       'PUT',
-      'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json',
+      'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes(),
       { reportProgress: true }
     );
@@ -19,9 +25,11 @@ export class DataStorageService {
   }
 
   getRecipes() {
+    const token = this.authService.getToken();
     this.http
       .get<Recipe[]>(
-        'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json',
+        'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json?auth=' +
+          token,
         {
           observe: 'body',
           responseType: 'json'
