463926567949c56676019c01b19395c63a788a85
Tomi Karikj <karikjtomi@gmail.com>
2019-02-10 17:46:44 +0100

    Create the full functionality behind the data storage

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const req = new HttpRequest(
+      'PUT',
+      'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(),
+      { reportProgress: true }
+    );
+    return this.http.request(req);
+  }
+
+  getRecipes() {
+    this.http
+      .get<Recipe[]>(
+        'https://ng-recipe-book-8a2f9.firebaseio.com/recipes.json',
+        {
+          observe: 'body',
+          responseType: 'json'
+        }
+      )
+      .pipe(
+        map(recipes => {
+          console.log(recipes);
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        })
+      )
+      .subscribe((recipes: Recipe[]) => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
