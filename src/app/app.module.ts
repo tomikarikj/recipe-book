109a2e58876ce4aa19cb3973065dbd47d0900f91
Tomi Karikj <karikjtomi@gmail.com>
2019-02-12 20:29:02 +0100

    Imported the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,16 @@ import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     AppRoutingModule
