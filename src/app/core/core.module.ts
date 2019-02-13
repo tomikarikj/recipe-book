4adaa9917d9c15c93e2832fda2daca4cdf52c7c2
Tomi Karikj <karikjtomi@gmail.com>
2019-02-13 19:14:05 +0100

    Added all the services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @NgModule({
   declarations: [HeaderComponent, HomeComponent],
   imports: [CommonModule, NgbModule, AppRoutingModule],
-  exports: [AppRoutingModule, HeaderComponent]
+  exports: [AppRoutingModule, HeaderComponent],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService
+  ]
 })
 export class CoreModule {}
