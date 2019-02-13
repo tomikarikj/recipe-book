f5d75f0f69ee7a608e2cc6fd395eed3b9b3effb9
Tomi Karikj <karikjtomi@gmail.com>
2019-02-13 19:14:17 +0100

    Removed the services and the auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
@@ -25,13 +19,6 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule,
     AppRoutingModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
